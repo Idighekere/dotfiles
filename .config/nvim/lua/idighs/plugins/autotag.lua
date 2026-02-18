ca96dff98f9f12d1fafc57db390d76dfae8dca96
Idighekere <idighekereudo@gmail.com>
2026-02-18 03:58:09 +0100

    add tailwind class folds and auto tag closing and renaming

---
+++ .config/nvim/lua/idighs/plugins/autotag.lua
@@ -0,0 +1,17 @@
+return {
+	"windwp/nvim-ts-autotag",
+	ft = { "html", "xml", "javascriptreact", "typescriptreact" },
+
+	config = function()
+		require("nvim-ts-autotag").setup({
+
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = false, -- Auto close on trailing </
+			},
+			per_filetype = {},
+		})
+	end,
+}
